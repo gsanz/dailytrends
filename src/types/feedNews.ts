84520175ad9a17db36e857a9c433b62c1c096d25
gsanz <gorsanmo@gmail.com>
2023-12-15 00:11:33 +0100

    postman crud

---
+++ src/types/feedNews.ts
@@ -0,0 +1,12 @@
+export type feedNews ={
+    headers: string[];
+    descriptions: string[];
+    dataSource: string;
+}
+
+export enum dataSourceEnum {
+    ELPAIS = 'El Pais',
+    ELNUNDO = 'El Mundo',
+}
+
+
